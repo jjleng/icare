e35759ec2f1c23e7d4d3a87eb8377318e6dbb4ab
Jason Leng <jason.leng@sv.cmu.edu>
2011-09-04 21:26:49 -0700

    Initial Repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Icare::Application.config.secret_token = '4788e2cedcbda8d860ba79c2857a58b6f2d6e74a164cdd9778e018da7bacf97f2d4943438351343ae0e45be9ae1071754bc0db38d77250416f06e3c8a782f7f2'
